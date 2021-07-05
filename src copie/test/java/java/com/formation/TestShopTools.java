33e8ad3cdb1f61794b4c751f48b63304e795cb0f
Mabachess <marc.lasserre@mconsultingweb.fr>
2021-07-05 15:17:45 +0200

    Initial Repo with copy-files & folders

---
+++ src copie/test/java/java/com/formation/TestShopTools.java
@@ -0,0 +1,38 @@
+package java.com.formation;
+
+import java.com.formationEpsi.pagesObject.DemoShopHomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.com.formationEpsi.pagesObject.DemoShopHomePage;
+
+public class TestShopTools {
+
+    public static void main(String[] args) {
+        //String projectPath = System.getProperty("user.dir");
+        //System.setProperty("webdriver.gecko.driver", projectPath + "\\drivers\\geckodriver\\geckodriver.exe");
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://shop.demoqa.com/");
+
+        /*WebElement searchLink = driver.findElement(By.className("noo-search"));
+        searchLink.click();
+        WebElement searchInputText = driver.findElement(By.name("s"));
+        searchInputText.sendKeys("dress");
+        searchInputText.submit();
+        List<WebElement> nbrArticle = driver.findElements(By.className("noo-product-inner"));
+        System.out.println(nbrArticle.size());
+        */
+        DemoShopHomePage demoShopHomePage = new DemoShopHomePage(driver);
+        demoShopHomePage.clickOnSearch();
+        demoShopHomePage.fillSearchInput("dress");
+        //Thread.sleep(1000); on attend le résultat pour quil compte
+        demoShopHomePage.nbProducts();
+
+    }
+}
---
+++ src copie/test/java/java/com/formation/TestShopToolsPrendreContact.java
@@ -0,0 +1,48 @@
+package java.com.formation;
+
+import java.com.formationEpsi.pagesObject.DemoShopHomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestShopToolsPrendreContact {
+
+    WebDriver driver;
+
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://shop.demoqa.com/");
+
+    }
+
+
+    @Test
+    public void prendreContact() {
+        DemoShopHomePage demoShop = new DemoShopHomePage(driver);
+
+        demoShop.attendreBoutonDismiss();
+        //assert a faire qui permet de dire que la fenetre s'est bien fermé
+        Assert.assertEquals("le champ cliquable", true, demoShop.prendreContactPhone().isEnabled());
+        Assert.assertTrue("", demoShop.prendreContactPhone().isDisplayed() && demoShop.prendreContactPhone().isEnabled());
+        demoShop.cliquerSurContactTelephone();
+        Assert.assertEquals("le champ cliquable", true,demoShop.prendreContactMail().isDisplayed() && demoShop.prendreContactMail().isEnabled() );
+        Assert.assertTrue("", demoShop.prendreContactPhone().isEnabled());
+        demoShop.cliquerSurContactMail();
+    }
+
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
